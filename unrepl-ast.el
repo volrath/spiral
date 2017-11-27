82a582a314163d5987cf75e327ff6198a8671de9
Daniel Barreto <daniel.barreto.n@gmail.com>
2017-11-27 13:16:28 +0100

    Implement muted-ui unparsing/tag readers
    Ideal for overlays

--- unrepl-ast.el
+++ unrepl-ast.el
@@ -73,11 +73,12 @@ Value is returned as an AST node."
                   (-partition 2 (parseclj-ast-children map-node)))))
 
 
-;; Unparsing
+
+;; Tag Readers
 ;; -------------------------------------------------------------------
 
 (defvar unrepl-ast-tag-readers
-  '((clojure/var . unrepl-ast--var-tag-unparse)
+  `((clojure/var . unrepl-ast--var-tag-unparse)
     (unrepl/... . unrepl-ast--elision-tag-unparse)
     (unrepl/mime . unrepl-ast--mime-tag-unparse)
     (unrepl/object . unrepl-ast--object-tag-unparse)
@@ -86,13 +87,25 @@ Value is returned as an AST node."
   "A set of tag readers for common UNREPL tagged literals.")
 
 
-(defun unrepl-ast--tag-child (tag-node)
-  "Return the child node of TAG-NODE."
-  (-> tag-node (parseclj-ast-children) (car)))
+(defun unrepl-ast--find-tag-reader (tag-symbol mute-ui raise-on-missing-tags)
+  "Find a tag reader for TAG-SYMBOL.
+This function looks for TAG-SYMBOL in `unrepl-ast-tag-readers'.
+MUTE-UI is a flag that indicates whether to return the main reader or the
+'no-ui' reader.  This flag get passed to each reader, so that it can decide
+what to do with the information.
+RAISE-ON-MISSING-TAGS is a flag that indicates whether to raise an error
+when no readers are found for TAG-SYMBOL."
+  (if-let (reader (map-elt unrepl-ast-tag-readers tag-symbol))
+      (lambda (tag)
+        (funcall reader tag mute-ui))
+    (when raise-on-missing-tags
+      (error "Missing tag: %S" tag-symbol))))
 
 
-(defun unrepl-ast--elision-tag-unparse (elision-tag-node &optional with-delimiters)
+(defun unrepl-ast--elision-tag-unparse (elision-tag-node mute-ui &optional with-delimiters)
   "Insert a generic elision button for ELISION-TAG-NODE.
+MUTE-UI is a flag that indicates whether or not to insert UI elements like
+buttons.
 WITH-DELIMITERS is a boolean value that indicate whether to keep or remove
 the opening and closing tokens for the retrieved
 collection (i.e. opening/closing parens for lists, opening/closing brackets
@@ -100,23 +113,27 @@ for vectors etc.)"
   (let ((elision-actions (unrepl-ast--tag-child elision-tag-node)))
     (pcase (parseclj-ast-node-type elision-actions)
       (:nil (delete-char -1))  ;; delete the trailing space leading to this node.
-      (:map (let ((get-more-action (-> elision-actions
-                                       (unrepl-ast-map-elt :get)
-                                       (unrepl-command-template))))
-              (unrepl-button-insert-one-off
-               unrepl-button-elision-label
-               get-more-action
-               (lambda (eval-payload)
-                 (unrepl-ast-unparse eval-payload (not with-delimiters))))))
+      (:map (if mute-ui
+                (insert (propertize "..." 'category 'unrepl-elision))
+              (let ((get-more-action (-> elision-actions
+                                         (unrepl-ast-map-elt :get)
+                                         (unrepl-command-template))))
+                (unrepl-button-insert-one-off
+                 unrepl-button-elision-label
+                 get-more-action
+                 (lambda (eval-payload)
+                   (unrepl-ast-unparse eval-payload (not with-delimiters)))))))
       (_ (when unrepl-debug
            (error "Unrecognized elision tagged form %S" elision-tag-node))))))
 
 
 (declare-function unrepl-attachment-find-handler "unrepl-attachment")
 (declare-function unrepl-attachment-insert-button "unrepl-attachment")
-(defun unrepl-ast--mime-tag-unparse (mime-tag-node)
-  "Insert a button to download any media in MIME-TAG-NODE."
-  (when unrepl-handle-rich-media
+(defun unrepl-ast--mime-tag-unparse (mime-tag-node mute-ui)
+  "Insert a button to download any media in MIME-TAG-NODE.
+If MUTE-UI is non-nil, don't do anything."
+  (when (and unrepl-handle-rich-media
+             (not mute-ui))
     (let* ((mime-spec (unrepl-ast--tag-child mime-tag-node))
            (content-type (parseclj-ast-value (unrepl-ast-map-elt mime-spec :content-type)))
            (attachment-handler (unrepl-attachment-find-handler content-type)))
@@ -127,28 +144,32 @@ for vectors etc.)"
                                              (unrepl-command-template)))))))
 
 
-(defun unrepl-ast--object-representation (type class id-hash repr &optional pretty-repr)
+(defun unrepl-ast--insert-object-representation (type class id-hash object-repr
+                                                      &optional pretty-repr)
   "Return a prettified object representation as a string.
 TYPE is a symbol that indicates the type of object (i.e object, function,
 atom, etc.)
 CLASS is the object's java class as a string.
 ID-HASH is the object's id hash as a string.
-REPR is the object's actual representation (as presented by
+OBJECT-REPR is the object's actual representation (as presented by
 clojure.main/repl) as a string.
 PRETTY-REPR is how UNREPL represents said object, i.e. functions come
 demunged from UNREPL."
   (let* ((pretty-repr (or pretty-repr class))
-         (actual-repr (format "#object[%s %S %s]" class id-hash repr)))
+         (actual-repr (format "#object[%s %S %s]" class id-hash object-repr)))
     (if (eql type 'object)
         actual-repr
-      (format "#%s[%s %s]"
-              type pretty-repr
-              (propertize id-hash
-                          'font-lock-face 'unrepl-font-tooltip-face
-                          'help-echo actual-repr)))))
+      (when (eql (current-buffer) (unrepl--make-buffer-for-mode 'clojure-mode))
+        (erase-buffer))
+      (insert
+       (format "#%s[%s %s]"
+               type pretty-repr
+               (propertize id-hash
+                           'font-lock-face 'unrepl-font-tooltip-face
+                           'help-echo actual-repr))))))
 
 
-(defun unrepl-ast--object-tag-unparse (object-tag-node)
+(defun unrepl-ast--object-tag-unparse (object-tag-node mute-ui)
   "Insert a string representation of OBJECT-TAG-NODE.
 OBJECT-TAG-NODE's child is a vector that has 4 elements, these are:
 - Class name, as a #unrepl.java/class tagged literal.
@@ -158,25 +179,24 @@ OBJECT-TAG-NODE's child is a vector that has 4 elements, these are:
 - A map node with extra information.
 
 By default, this function will create tooltips for the object
-representation with additional information.  DISABLE-UI overrides this
+representation with additional information.  MUTE-UI overrides this
 behavior."
   (let* ((obj-attrs (parseclj-ast-children (unrepl-ast--tag-child object-tag-node)))
+         (class-name (unrepl-ast-unparse-to-string (car obj-attrs)))
+         (id-hash (parseclj-ast-value (cadr obj-attrs)))
          (object-repr "--"))  ;; TODO: placeholder for actual object repr
     (if (not unrepl-enable-pretty-object-representations)
-        object-repr
-      (let* ((class-name (unrepl-ast-unparse-to-string (car obj-attrs)))
-             (id-hash (parseclj-ast-value (cadr obj-attrs)))
-             (unrepl-repr-node (cl-caddr obj-attrs))
+        (unrepl-ast--insert-object-representation 'object class-name id-hash object-repr)
+      (let* ((unrepl-repr-node (cl-caddr obj-attrs))
              (unrepl-repr (unrepl-ast-unparse-to-string unrepl-repr-node)))
         (cond
          ;; Representation for a "demunged" function.
          ((eql (parseclj-ast-node-type unrepl-repr-node) :symbol)
-          (insert
-           (unrepl-ast--object-representation 'function
-                                              class-name
-                                              id-hash
-                                              object-repr
-                                              unrepl-repr)))
+          (unrepl-ast--insert-object-representation 'function
+                                                    class-name
+                                                    id-hash
+                                                    object-repr
+                                                    unrepl-repr))
          ;; Image
          ((and (eql (parseclj-ast-node-type unrepl-repr-node) :map)
                (unrepl-ast-map-elt unrepl-repr-node :attachment)
@@ -188,22 +208,23 @@ behavior."
                              (unrepl-ast-map-elt :height)
                              (unrepl-ast-unparse-to-string)))
                  (attachment (unrepl-ast-map-elt unrepl-repr-node :attachment)))
-            (insert
-             (unrepl-ast--object-representation 'image class-name id-hash unrepl-repr
-                                                (format "%s <%sx%s>" class-name width height)))
-            (unrepl-ast-unparse attachment)))
+            (unrepl-ast--insert-object-representation 'image class-name id-hash object-repr
+                                                      (format "%s <%sx%s>" class-name width height))
+            (unless mute-ui
+              (unrepl-ast-unparse attachment))))
          ;; Any other type of object
-         (t (insert
-             (unrepl-ast--object-representation
-              'object class-name id-hash unrepl-repr))))))))
+         (t (unrepl-ast--insert-object-representation
+             'object class-name id-hash unrepl-repr)))))))
 
 
-(defun unrepl-ast--string-tag-unparse (string-tag-node &optional stdout-str)
+(defun unrepl-ast--string-tag-unparse (string-tag-node mute-ui &optional stdout-str)
   "Insert a string representation of STRING-TAG-NODE.
 STDOUT-STR is a boolean value that indicates whether or not the
 STRING-TAG-NODE represents a stdout (`:out') message from UNREPL.  In case
 that STDOUT-STR is non-nil, surronding quotes for this string will be
-ommited."
+ommited.
+MUTE-UI is a flag that indicates whether or not to insert UI elements like
+buttons."
   (let* ((insert-fn (if stdout-str
                         #'unrepl-ast-unparse-stdout-string
                       #'unrepl-ast-unparse))
@@ -215,48 +236,65 @@ ommited."
     ;; insert string
     (funcall insert-fn string-node)
     ;; and then elision button
-    (unrepl-button-insert-one-off
-     unrepl-button-elision-label
-     (-> elision-actions
-         (unrepl-ast-map-elt :get)
-         (unrepl-command-template))
-     (lambda (eval-payload)
-       (let ((p (point)))
-         (funcall insert-fn eval-payload)
-         (unless stdout-str  ;; Delete the opening quote
-           (delete-region p (1+ p)))))
-     nil
-     (unless stdout-str (1- (point))))))
+    (if mute-ui
+        (insert (propertize "..." 'category 'unrepl-elision))
+      (unrepl-button-insert-one-off
+       unrepl-button-elision-label
+       (-> elision-actions
+           (unrepl-ast-map-elt :get)
+           (unrepl-command-template))
+       (lambda (eval-payload)
+         (let ((p (point)))
+           (funcall insert-fn eval-payload)
+           (unless stdout-str  ;; Delete the opening quote
+             (delete-region p (1+ p)))))
+       nil
+       (unless stdout-str (1- (point)))))))
 
 
-(defun unrepl-ast--var-tag-unparse (var-tag-node)
+(defun unrepl-ast--var-tag-unparse (var-tag-node _mute-ui)
   "Insert a string representation of VAR-TAG-NODE."
-  (unrepl-ast--generic-tag-child-unparse var-tag-node))
+  (unrepl-ast--generic-tag-child-unparse var-tag-node nil))
 
 
-(defun unrepl-ast--class-tag-unparse (class-tag-node)
+(defun unrepl-ast--class-tag-unparse (class-tag-node _mute-ui)
   "Insert a string representation of a java class CLASS-TAG-NODE."
   (unrepl-propertize-region '(font-lock-face unrepl-font-class-name-face)
-    (unrepl-ast--generic-tag-child-unparse class-tag-node)))
+    (unrepl-ast--generic-tag-child-unparse class-tag-node nil)))
 
 
-(defun unrepl-ast--generic-tag-child-unparse (tag-node)
-  "Skip TAG-NODE's name and unparse its child."
+(defun unrepl-ast--generic-tag-child-unparse (tag-node mute-ui)
+  "Skip TAG-NODE's name and unparse its child.
+MUTE-UI is a flag that indicates whether or not to insert UI elements like
+buttons."
   (-> tag-node
       (unrepl-ast--tag-child)
-      (unrepl-ast-unparse)))
+      (unrepl-ast-unparse nil mute-ui)))
 
 
-(defun unrepl-ast--generic-tag-unparse (tag-node)
-  "Insert a string representation of the given AST TAG-NODE into buffer."
+(defun unrepl-ast--generic-tag-unparse (tag-node mute-ui)
+  "Insert a string representation of the given AST TAG-NODE into buffer.
+MUTE-UI is a flag that indicates whether or not to insert UI elements like
+buttons."
   (insert "#" (symbol-name (parseclj-ast-node-attr tag-node :tag)) " ")
-  (unrepl-ast--generic-tag-child-unparse tag-node))
+  (unrepl-ast--generic-tag-child-unparse tag-node mute-ui))
 
 
-(defun unrepl-ast--unparse-collection (node no-delimiters)
+
+;; Unparsing
+;; -------------------------------------------------------------------
+
+(defun unrepl-ast--tag-child (tag-node)
+  "Return the child node of TAG-NODE."
+  (-> tag-node (parseclj-ast-children) (car)))
+
+
+(defun unrepl-ast--unparse-collection (node no-delimiters mute-ui raise-on-missing-tags)
   "Insert a string representation of the given AST branch NODE into buffer.
 When NO-DELIMITERS is non-nil, avoid inserting delimiters for this
-collection."
+collection.
+MUTE-UI and RAISE-ON-MISSING-TAGS are both flags, as described in
+`unrepl-ast-unparse'."
   (let* ((token-type (parseclj-ast-node-type node))
          (delimiters (cl-case token-type
                        (:root (cons "" ""))
@@ -267,11 +305,11 @@ collection."
     (unless no-delimiters (insert (car delimiters)))
     (let ((nodes (parseclj-ast-children node)))
       (when-let (node (car nodes))
-        (unrepl-ast-unparse node))
+        (unrepl-ast-unparse node nil mute-ui raise-on-missing-tags))
       (seq-doseq (child (cdr nodes))
         (when (null (parseclj-ast-node-attr node :lexical-preservation))  ;; hack, read below.
           (insert " "))
-        (unrepl-ast-unparse child)))
+        (unrepl-ast-unparse child nil mute-ui raise-on-missing-tags)))
     (unless no-delimiters (insert (cdr delimiters)))))
 ;; Here we're basically accessing parseclj's internal API to check if a node has
 ;; lexical preservation.  It would be better if parseclj would expose a function
@@ -281,7 +319,7 @@ collection."
 ;; scope.
 
 
-(defun unrepl-ast-unparse (ast-node &optional no-delimiters raise-on-missing-tags)
+(defun unrepl-ast-unparse (ast-node &optional no-delimiters mute-ui raise-on-missing-tags)
   "Unparse a parseclj AST-NODE into a human-friendly representation.
 For the most parts, this function behaves exactly as
 `parseclj-unparse-clojure-to-string', but it will also consume all tagged
@@ -291,6 +329,8 @@ NO-DELIMITERS is a boolean value that indicate whether to delete/remove
 the opening and closing tokens for the retrieved
 collection (i.e. opening/closing parens for lists, opening/closing brackets
 for vectors etc.)
+MUTE-UI is a boolean-value to indicate whether or not to include UI
+elements like buttons into the unparsing.
 If AST-NODE contains a tagged literal not represented in TAG-READERS-MAP,
 an error will be raised.  RAISE-ON-MISSING-TAGS overrides this behavior.
 Return a string that may or may not be propertized, and may or may not
@@ -299,38 +339,40 @@ include other UI elements."
       (insert (parseclj-ast-node-attr ast-node :form))
     (if (eql (parseclj-ast-node-type ast-node) :tag)
         (let* ((tag-symbol (parseclj-ast-node-attr ast-node :tag))
-               (reader (map-elt unrepl-ast-tag-readers tag-symbol)))
+               (reader (unrepl-ast--find-tag-reader tag-symbol mute-ui raise-on-missing-tags)))
           (if reader
               (funcall reader ast-node)
-            (if raise-on-missing-tags
-                (error "Missing tag %s" tag-symbol)
-              (unrepl-ast--generic-tag-unparse ast-node))))
-      (unrepl-ast--unparse-collection ast-node no-delimiters))))
+            (unrepl-ast--generic-tag-unparse ast-node mute-ui)))
+      (unrepl-ast--unparse-collection ast-node no-delimiters mute-ui raise-on-missing-tags))))
 
 
-(defun unrepl-ast-unparse-to-string (ast-node &optional raise-on-missing-tags)
+(defun unrepl-ast-unparse-to-string (ast-node &optional with-ui raise-on-missing-tags)
   "Unparse a parseclj AST-NODE to a string representation.
-This function works like `unrepl-ast-unparse' but avoids adding UI
-elements (like buttons) to the returned string.
+This function works like `unrepl-ast-unparse' but returns a string.
+WITH-UI is a flag to indicate whether or not to include UI elements like
+buttons in the result.  It's defined as the complement of
+`unrepl-ast-unparse's NO-UI flag, since it is commonly expected to not want
+UI elements when rendering to a string.
 If AST-NODE contains tagged literals not represented in TAG-READERS-MAP, an
 error will be raised.  RAISE-ON-MISSING-TAGS overrides this behavior.
 The returned string will be automatically font-locked as clojure code."
   (with-current-buffer (unrepl--make-buffer-for-mode 'clojure-mode)
-    ;; TODO: add a flag to mute UI elements
     (erase-buffer)
-    (unrepl-ast-unparse ast-node raise-on-missing-tags)
+    (unrepl-ast-unparse ast-node nil (not with-ui) raise-on-missing-tags)
     (font-lock-fontify-region (point-min) (point-max))
     (buffer-string)))
 
 
-(defun unrepl-ast-unparse-stdout-string (string-node)
-  "Unparse STRING-NODE, which can be a simple string or a #unrepl/string tag."
+(defun unrepl-ast-unparse-stdout-string (string-node &optional mute-ui)
+  "Unparse STRING-NODE, which can be a simple string or a #unrepl/string tag.
+MUTE-UI is a flag to indicate whether or not insert UI elements like
+buttons."
   (if (eql (parseclj-ast-node-type string-node) :string)
       (-> string-node
           (parseclj-ast-value)
           (propertize 'font-lock-face 'unrepl-font-stdout-face)
           (insert))
-    (unrepl-ast--string-tag-unparse string-node t)))
+    (unrepl-ast--string-tag-unparse string-node mute-ui t)))
 
 
 ;; Template Management
